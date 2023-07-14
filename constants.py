71b5ac196381741ae88cf3b95669dc315ae52caa
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-07-14 17:01:32 -0300

    class Block created which represents each singular block that forms other pieces. Class square also created. SHould create abstract class piece later

--- constants.py
+++ constants.py
@@ -3,16 +3,19 @@
 SCREEN_WIDTH: int = 750
 SCREEN_HEIGHT: int = 750
 
-BOARD_WIDTH: int = 450
+BOARD_WIDTH: int = 400
 BOARD_HEIGHT: int = 600
-BOARD_UPPER_LEFT_POS: tuple[int, int] = (100, 100)
+BOARD_UPPER_LEFT_POS: tuple[int, int] = (100, 50)
 FPS: int = 60
+
+BLOCK_SIZE: int = 25
 
 
 # colors
 
-WHITE: tuple[int, int, int] = (255,  255,  255)
+WHITE: tuple[int, int, int] = (255, 255, 255)
 BLACK: tuple[int, int, int] = (0, 0, 0)
 RED: tuple[int, int, int] = (255, 0, 0)
 BLUE: tuple[int, int, int] = (0, 255, 0)
 GREEN: tuple[int, int, int] = (0, 0, 255)
+GREY: tuple[int, int, int] = (189, 183, 183)
--- main.py
+++ main.py
@@ -4,20 +4,58 @@ pygame.init()
 
 from constants import *
 
+from piece import Block, Square
+
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tetris")
+
+
+def draw_board(
+    board_upper_left_pos: tuple[int, int], board_width: int, board_height: int
+) -> None:
+    rect = pygame.Rect(*board_upper_left_pos, BOARD_WIDTH, BOARD_HEIGHT)
+    pygame.draw.rect(SCREEN, GREY, rect, width=1)
+
+    # grid for helping debugging
+    for i in range(BOARD_WIDTH // BLOCK_SIZE):
+        pygame.draw.line(
+            SCREEN,
+            GREY,
+            (board_upper_left_pos[0] + BLOCK_SIZE * i, board_upper_left_pos[1]),
+            (
+                board_upper_left_pos[0] + BLOCK_SIZE * i,
+                board_upper_left_pos[1] + BOARD_HEIGHT,
+            ),
+        )
+    for i in range(BOARD_HEIGHT // BLOCK_SIZE):
+        pygame.draw.line(
+            SCREEN,
+            GREY,
+            (board_upper_left_pos[0], board_upper_left_pos[1] + BLOCK_SIZE * i),
+            (
+                board_upper_left_pos[0] + BOARD_WIDTH,
+                board_upper_left_pos[1] + BLOCK_SIZE * i,
+            ),
+        )
 
 
 def main():
     run: bool = True
     clock: pygame.time.Clock = pygame.time.Clock()
+    pieces_on_screen: list = [Square(color=RED)]
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         pygame.display.update()
-    
+        draw_board(
+            board_upper_left_pos=BOARD_UPPER_LEFT_POS,
+            board_width=BOARD_WIDTH,
+            board_height=BOARD_HEIGHT,
+        )
+        for piece in pieces_on_screen:
+            piece.draw(screen=SCREEN)
     pygame.quit()
 
 
